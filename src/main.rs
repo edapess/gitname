ad234fd5daa7fd36507a901130d6e0b1ade34b1b
edapess <edmondavetisyanw@gmail.com>
2023-12-29 19:22:21 +0400

    feat: add create and checkout logic for git branch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process::Command;
 
 fn git_name() -> Vec<String> {
     let mut guess = String::new();
@@ -9,6 +10,46 @@ fn git_name() -> Vec<String> {
 
     let array: Vec<String> = guess.trim().split('/').map(String::from).collect();
     array
+}
+fn create_branch_with_name (branch_name_from_link: String) {
+ // create a new Git branch named "branch_name_from_link"
+ let branch_output = Command::new("git")
+ .arg("branch")
+ .arg(branch_name_from_link.clone())
+ .output()
+ .expect("Failed to execute 'git branch' command");
+
+// check if creating the branch was successful
+if branch_output.status.success() {
+ println!("Git branch {branch_name_from_link} created successfully");
+
+ // switch to the new Git branch
+ let checkout_output = Command::new("git")
+     .arg("checkout")
+     .arg(branch_name_from_link.clone())
+     .output()
+     .expect("Failed to execute 'git checkout' command");
+
+ // Print the output of the checkout command (if any)
+ if !checkout_output.stdout.is_empty() {
+     println!("stdout: {}", String::from_utf8_lossy(&checkout_output.stdout));
+ }
+
+ // Print the error of the checkout command (if any)
+ if !checkout_output.stderr.is_empty() {
+     eprintln!("stderr: {}", String::from_utf8_lossy(&checkout_output.stderr));
+ }
+
+ // Check if the checkout command was successful
+ if checkout_output.status.success() {
+     println!("Switched to Git branch '{branch_name_from_link} successfully");
+ } else {
+     eprintln!("Failed to switch to Git branch");
+ }
+} else {
+ eprintln!("Failed to create Git branch");
+}
+
 }
 
 fn main() {
@@ -22,9 +63,8 @@ fn main() {
                 s.to_string()
             }
         }).collect::<Vec<String>>().join("-");
-        println!("{:?}", branch_name);
+        create_branch_with_name(branch_name);
     } else {
         println!("please check your string");
     }
-
 }
